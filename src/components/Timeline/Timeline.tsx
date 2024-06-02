53a424757f5449b08d7b5bd5375d8c979a55df39
martinjedwabny <martiniedva@gmail.com>
2024-06-02 10:57:33 +0200

    refactor: separate TimelineLine from Timeline

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -5,6 +5,7 @@ import {
     useScroll,
     useSpring,
     useMotionValueEvent,
+    MotionValue,
 } from "framer-motion";
 import { cn } from "@/utils/cn";
 
@@ -24,7 +25,7 @@ export const Timeline = ({
     });
 
     const contentRef = useRef<HTMLDivElement>(null);
-    const [svgHeight, setSvgHeight] = useState(0);
+    const [svgHeight, setSvgHeight] = useState(0); 
 
     useEffect(() => {
         if (contentRef.current) {
@@ -47,58 +48,18 @@ export const Timeline = ({
         }
     );
 
-    useEffect(() => {
-        console.log(scrollYProgress.get(), y1.get(), y2.get());
-    }, [scrollYProgress, y1, y2]);
-
     return (
         <motion.div
             ref={ref}
             className={cn("relative w-full max-w-4xl mx-auto h-full flex flex-row", className)}
         >
             <div className="mt-5 max-md:ml-[-10px]">
-                <svg
-                    viewBox={`0 0 20 ${svgHeight}`}
-                    width="20"
-                    height={svgHeight} // Set the SVG height
-                    className="block"
-                    aria-hidden="true"
-                >
-                    <motion.path
-                        d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.8} 24V ${svgHeight}`}
-                        fill="none"
-                        stroke="#9091A0"
-                        strokeOpacity="0.36"
-                        transition={{
-                            duration: 10,
-                        }}
-                    ></motion.path>
-                    <motion.path
-                        d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.8} 24V ${svgHeight}`}
-                        fill="none"
-                        stroke={`url(#gradient${id})`}
-                        strokeWidth="1.25"
-                        className="motion-reduce:hidden"
-                        transition={{
-                            duration: 10,
-                        }}
-                    ></motion.path>
-                    <defs>
-                        <motion.linearGradient
-                            id={`gradient${id}`}
-                            gradientUnits="userSpaceOnUse"
-                            x1="0"
-                            x2="0"
-                            y1={y1} // set y1 for gradient
-                            y2={y2} // set y2 for gradient
-                        >
-                            <stop stopColor="#18CCFC" stopOpacity="0"></stop>
-                            <stop stopColor="#18CCFC"></stop>
-                            <stop offset="0.325" stopColor="#6344F5"></stop>
-                            <stop offset="1" stopColor="#AE48FF" stopOpacity="0"></stop>
-                        </motion.linearGradient>
-                    </defs>
-                </svg>
+                <TimelineLine
+                    svgHeight={svgHeight}
+                    id={id}
+                    y1={y1}
+                    y2={y2}
+                />
             </div>
             <div ref={contentRef} className="flex-auto">
                 {items.map((item, index) => (
@@ -113,6 +74,61 @@ export const Timeline = ({
         </motion.div>
     );
 };
+
+function TimelineLine({
+    svgHeight,
+    id,
+    y1,
+    y2
+}: {
+    svgHeight: number;
+    id: string;
+    y1: MotionValue;
+    y2: MotionValue;
+    }) {
+    return <svg
+        viewBox={`0 0 20 ${svgHeight}`}
+        width="20"
+        height={svgHeight} // Set the SVG height
+        className="block"
+        aria-hidden="true"
+    >
+        <motion.path
+            d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.8} 24V ${svgHeight}`}
+            fill="none"
+            stroke="#9091A0"
+            strokeOpacity="0.36"
+            transition={{
+                duration: 10,
+            }}
+        ></motion.path>
+        <motion.path
+            d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.8} 24V ${svgHeight}`}
+            fill="none"
+            stroke={`url(#gradient${id})`}
+            strokeWidth="1.25"
+            className="motion-reduce:hidden"
+            transition={{
+                duration: 10,
+            }}
+        ></motion.path>
+        <defs>
+            <motion.linearGradient
+                id={`gradient${id}`}
+                gradientUnits="userSpaceOnUse"
+                x1="0"
+                x2="0"
+                y1={y1} // set y1 for gradient
+                y2={y2} // set y2 for gradient
+            >
+                <stop stopColor="#18CCFC" stopOpacity="0"></stop>
+                <stop stopColor="#18CCFC"></stop>
+                <stop offset="0.325" stopColor="#6344F5"></stop>
+                <stop offset="1" stopColor="#AE48FF" stopOpacity="0"></stop>
+            </motion.linearGradient>
+        </defs>
+    </svg>;
+}
 
 function TimelineDot({ className }: { className?: string }) {
     const ref = useRef<HTMLDivElement>(null);
